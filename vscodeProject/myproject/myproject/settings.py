0409a1f3e6fa4e620e4d7282d68b8eb66722f632
SACHIN PRAJAPATI <skprajapati3214@gmail.com>
2024-06-24 18:02:02 +0530

    template settings

--- vscodeProject/myproject/myproject/settings.py
+++ vscodeProject/myproject/myproject/settings.py
@@ -59,7 +59,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
